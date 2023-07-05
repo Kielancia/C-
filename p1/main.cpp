a1e189d348c8d856958ec3862efbb20cf2dccd30
Kielancia <mysweetnaivety@gmail.com>
2023-07-05 19:23:48 +0200

    Create program no.2
    Getting a number from the user

---
+++ p1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int favoriteNumber;
+    cout << "Enter you favorite number between 1 and 100: " << endl;
+    cin >> favoriteNumber;
+    cout << "Amazing! That's my favorite number too!" << endl;
+
+    return 0;
+}
