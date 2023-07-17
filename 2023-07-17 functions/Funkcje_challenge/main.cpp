c0644e2e76639a071d5f20990ef430cdbe0077a2
Kielancia <mysweetnaivety@gmail.com>
2023-07-17 20:50:20 +0200

    Created function program
    Panel with options to choose for user

---
+++ 2023-07-17 functions/Funkcje_challenge/main.cpp
@@ -0,0 +1,15 @@
+/*
+    Your challenge for section 11 is to modularize your solution
+    to the Section 9 challenge by refactoring your solution so that
+    it uses uses functions.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
